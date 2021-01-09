a93a50e6f27d0d7dfd0e6162aa4af0f6e20417aa
cunnellp5 <philip.cunnell@colorado.edu>
2021-01-09 12:41:05 -0700

    Setup FC main website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import GlitchImage from './components/GlitchImage/GlitchImage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <GlitchImage></GlitchImage>
       </header>
     </div>
   );
---
+++ src/components/GlitchImage/GlitchImage.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import logo from '../../logo2.gif';
+import './GlitchImage.css';
+
+const GlitchImage = () => {
+  return (
+    <img src={logo} className="logo" alt="logo" />
+  );
+}
+
+export default GlitchImage;
