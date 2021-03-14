3c87f5f3ddc6198b516885c86ce11840684a23d7
cunnellp5 <philip.cunnell@colorado.edu>
2021-03-14 13:29:50 -0600

    Add new gifs

--- src/index.js
+++ src/index.js
@@ -15,12 +15,27 @@ const logo2 = STORAGE_REF.child('logo2.gif')
 const trippycolorful = STORAGE_REF.child('trippycolorful.gif')
 const pixel = STORAGE_REF.child('pixel.gif')
 const prettylines = STORAGE_REF.child('prettylines.gif')
+const rainbowresample = STORAGE_REF.child('rainbowresample.gif')
+const icy = STORAGE_REF.child('icy.gif')
+// const brainbowgif = STORAGE_REF.child('brainbowgif.gif') // make the bkgrnd black!icy
+
+
+// todo figure out a way to get the items out of this
+
+// STORAGE_REF.listAll().then( res => {
+//   res.items.forEach( item => {
+//     console.log(item.name)
+//   });
+// })
 
 export const logoMap = {
   0: logo2,
   1: trippycolorful,
   2: pixel,
-  3: prettylines
+  3: prettylines,
+  4: rainbowresample,
+  5: icy,
+  // 5: brainbowgif, // todo make the bkg black
 }
 
 ReactDOM.render(
